9dd68823e7c8e077e1ae67dfc57f5659e851e936
Una Paruma <parumauna@gmail.com>
2023-03-20 19:28:20 +0100

    first post

---
+++ models/productModels.js
@@ -0,0 +1,26 @@
+const mongoose = require ('mongoose');
+
+const productSchema = mongoose.Schema(
+    {
+        name:{
+            type: String,
+            required: [true, "Please enter a product name"]
+        },
+        quantity:{
+            type:Number,
+            required: true,
+            default: 0
+        },
+        price:{
+            type:Number,
+            required: true,
+        }
+    },
+    {
+        timeStamps: true
+    }
+)
+
+const Product = mongoose.model('product', productSchema);
+
+module.exports = Product;
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require ('mongoose');
+const Product = require('./models/productModels');
 const app = express();
+
+app.use(express.json);
 
 //routes
 
@@ -10,10 +13,24 @@ app.get('/', (req,res) =>{
 app.get('/blog', (req,res) =>{
     res.send('Hello blog')
 })
+app.post('/product', async(req,res) =>{
+   try{
+    const product = await Product.create(req.body)
+    res.status(200).json(product);
 
-
-app.listen(3000, ()=>{
-    console.log('Node API app is running on port 3000')
+   } catch (error) {
+    console.log(error.message);
+    res.status(500).json({message: error.message})
+   }
 })
 
+
 mongoose.connect('mongodb+srv://admin123:admin123@cluster0.7wpcxfc.mongodb.net/stadfirma?retryWrites=true&w=majority')
+.then(() => {
+    app.listen(3000, ()=>{
+        console.log('Node API app is running on port 3000')
+    });
+    console.log('Conected to MongoDB');
+}).catch((error) => {
+    console.log(error);
+})
