da67565a757a6ccebd3654f04e5d361f5ff9f78b
Una Paruma <parumauna@gmail.com>
2023-03-20 17:53:22 +0100

    database conected

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
+const mongoose = require ('mongoose');
 const app = express();
 
 //routes
 
 app.get('/', (req,res) =>{
-    res.send('hello node API')
+    res.send('Hello node API')
+})
+app.get('/blog', (req,res) =>{
+    res.send('Hello blog')
 })
 
 
 app.listen(3000, ()=>{
-    console.log('node API app is running on port 3000')
+    console.log('Node API app is running on port 3000')
 })
+
+mongoose.connect('mongodb+srv://admin123:admin123@cluster0.7wpcxfc.mongodb.net/stadfirma?retryWrites=true&w=majority')
