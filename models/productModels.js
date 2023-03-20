2ac626494e029287d88138b1b562623f557f56d2
Una Paruma <parumauna@gmail.com>
2023-03-20 21:17:13 +0100

    bug fixes

--- models/productModels.js
+++ models/productModels.js
@@ -1,26 +1,31 @@
-const mongoose = require ('mongoose');
+const mongoose = require('mongoose')
 
 const productSchema = mongoose.Schema(
     {
-        name:{
+        name: {
             type: String,
             required: [true, "Please enter a product name"]
         },
-        quantity:{
-            type:Number,
-            required: true,
+        quantity: {
+            type: Number,
+            required: false,
             default: 0
         },
-        price:{
-            type:Number,
-            required: true,
+        price: {
+            type: Number,
+            required: false,
+        },
+        image: {
+            type: String,
+            required: false,
         }
     },
     {
-        timeStamps: true
+        timestamps: true
     }
 )
 
-const Product = mongoose.model('product', productSchema);
+
+const Product = mongoose.model('Product', productSchema);
 
 module.exports = Product;
